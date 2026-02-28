34a32742a3c2c59affea2a4eceb184dda0a4857f
poojapatil-debug <poojachapatil@gmail.com>
2026-02-28 18:29:09 +0530

    poo commited

--- src/test/java/com/example/Spring_boot_jenkins/SpringBootJenkinsApplication.java
+++ src/test/java/com/example/Spring_boot_jenkins/SpringBootJenkinsApplication.java
@@ -4,8 +4,9 @@ import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @Slf4j
 public class SpringBootJenkinsApplication {
 
